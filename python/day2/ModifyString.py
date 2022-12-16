c3757c91007a69b04b8fc0116d09a4d2ef3ce2ea
azatbayram <bayramov.azat.tkm@gmail.com>
2022-12-16 08:25:12 +0300

    modify string

---
+++ python/day2/ModifyString.py
@@ -0,0 +1,19 @@
+txt="Hello World!"
+# upper case
+print(txt.upper())
+
+# lower case
+print(txt.lower())
+
+# sprit remove whitespace from beginning and end
+x=" I am Azat "
+print(x.strip())
+
+#  replace method
+print(txt.replace("H", "J"))
+print(txt.replace("World","Python"))
+
+# split method
+print(txt.split(" "))
+y="Azat 26"
+print(y.split())
