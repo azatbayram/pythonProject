dac32fc2251af1259a3c1cfccc9a5abacb8fd652
azatbayram <bayramov.azat.tkm@gmail.com>
2022-12-17 16:45:42 +0300

    access tuple

--- python/day4/AccessTuple.py
+++ python/day4/AccessTuple.py
@@ -10,3 +10,7 @@ print(myTuple[2:5])
 print(myTuple[:6])
 print(myTuple[2:])
 print(myTuple[-4:-2])
+
+thistuple = ("apple", "banana", "cherry")
+if "apple" in thistuple:
+  print("Yes, 'apple' is in the fruits tuple")
