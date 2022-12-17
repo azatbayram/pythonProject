ce59f02f89f9673b62155b3bfa2f3bf3c2c800ae
azatbayram <bayramov.azat.tkm@gmail.com>
2022-12-17 17:25:25 +0300

    join tuple

---
+++ python/day4/JoinTuple.py
@@ -0,0 +1,15 @@
+# join two tuple with + operator
+tuple1=("a", "b", "c")
+tuple2=(1, 2, 3)
+
+tuple3=tuple1+tuple2
+
+print(tuple3)
+
+# mutliply tuple
+
+fruits=("apple", "banana", "cherry")
+
+myTuple=fruits*2
+
+print(myTuple)
