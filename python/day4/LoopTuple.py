3f169010f90e3d6b2b9ec115749993c4e637e27e
azatbayram <bayramov.azat.tkm@gmail.com>
2022-12-17 17:19:34 +0300

    loop tuple

---
+++ python/day4/LoopTuple.py
@@ -0,0 +1,22 @@
+# loop tuple with for loop
+myTuple=("apple", "banana", "cherry")
+
+for x in myTuple:
+    print(x)
+
+print("------------------------")
+
+for i in range(len(myTuple)):
+    print(myTuple[i])
+
+print("------------------------")
+
+# while loop
+i=0
+
+while i<len(myTuple):
+    print(myTuple[i])
+    i+=1
+
+
+
