c1531ccad437ffee5c5f401db350c8afebea1d85
azatbayram <bayramov.azat.tkm@gmail.com>
2022-12-17 16:41:17 +0300

    create tuple

---
+++ python/day4/CreateTuple.py
@@ -0,0 +1,15 @@
+myTuple=("apple", "banana", "cherry", "apple")
+print(myTuple)
+
+# length of tuple
+print(len(myTuple))
+
+tuple1 = ("apple", "banana", "cherry")
+tuple2 = (1, 5, 7, 9, 3)
+tuple3 = (True, False, False)
+tuple4 = ("abc", 34, True, 40, "male")
+
+print(tuple1)
+print(tuple2)
+print(tuple3)
+print(tuple4)
