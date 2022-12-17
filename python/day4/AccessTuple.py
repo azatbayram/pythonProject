031c2d210202e2b66c6ff93c8d19af3653379abb
azatbayram <bayramov.azat.tkm@gmail.com>
2022-12-17 16:45:05 +0300

    access tuple

---
+++ python/day4/AccessTuple.py
@@ -0,0 +1,12 @@
+# access tuple items with index
+myTuple=("apple", "banana", "cherry", "orange", "kiwi", "melon", "mango")
+print(myTuple[2])
+
+# access with negative index
+print(myTuple[-3])
+
+# range of index
+print(myTuple[2:5])
+print(myTuple[:6])
+print(myTuple[2:])
+print(myTuple[-4:-2])
