d3b8dbbd691210ecfa105a8dd1f72b2121bb4384
azatbayram <bayramov.azat.tkm@gmail.com>
2022-12-17 17:14:58 +0300

    unpacking tuple

---
+++ python/day4/UnpackingTuple.py
@@ -0,0 +1,22 @@
+fruits=("apple", "banana", "cherry") # this is packing
+
+(green, yellow, red)=fruits # this unpacking
+print(green)
+print(yellow)
+print(red)
+
+fruits = ("apple", "banana", "cherry", "strawberry", "raspberry")
+
+(green, yellow, *red) = fruits
+
+print(green)
+print(yellow)
+print(red)
+
+fruits = ("apple", "mango", "papaya", "pineapple", "cherry")
+
+(green, *tropic, red) = fruits
+
+print(green)
+print(tropic)
+print(red)
